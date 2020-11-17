265d91f121e489c255e7c732bfb44a5b413846d0
ecandotti <enzo.candotti16@hotmail.com>
2020-11-17 17:39:30 +0100

    Gestion des deux View + Optimisation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,106 +1,39 @@
-import user  from '../img/man.svg'
-import edit from '../img/icon/edit.svg'
-import icon_trash from '../img/icon/trash.svg'
+import Grid from './Grid'
+import List from './List'
 
 const Card = (props) => {
 
-    const name = props.student.name
-    const lastName = props.student.lastName
-    const speciality = props.student.lastName
-
-    return (
-        <>
-            <div className="card">
-                <div className="right">
-                    <img src={user} alt=""/>
-                </div>
-                <div className="left">
-                    <h3> {name}  {lastName}</h3>
-                    <p> {speciality}</p>
-                </div>
-                <div className="top">
-                    <div className="delete">
-                        <a href='#'>
-                            <img src={icon_trash} alt=""/>
-                        </a>
-                    </div>
-
-                    <div className="edit">
-                        <a href='#'>
-                            <img src={edit} alt=""/>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
-
-
-
-const List = (props) => {
-
-    const name = props.student.name
-    const lastName = props.student.lastName
-    const speciality = props.student.speciality
-    const id = props.student.id
-
-    return(
-            <tr>    
-                <td>
-                     <div className="delete">
-                        <a href='#'>
-                            <img src={icon_trash} alt=""/>
-                        </a>
-                    </div>
-
-                    <div className="edit">
-                        <a href='#'>
-                            <img src={edit} alt=""/>
-                        </a>
-                    </div>
-                </td>
-
-                <td>{id}</td>
-
-                <td><img src={user} alt=""/></td>
-                <td>{name}</td>
-                <td>{lastName}</td>
-                <td>{speciality}</td>
-            </tr>
-    )
-}
-
-
-const Cards = (props) => {
-
-    console.log(props)
-    
-    const students = props.students
     const moded = props.showListGrid
-    let listItems
 
-    if(moded) {
-        let enAtt = students.map((student) => <List student={student}/>)
+    if( moded ) {
 
-        listItems = <table className="list">   
-                    <tr>
-                        <th></th>
-                        <th>id</th>
-                        <th>Image</th>
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>Spécialité</th>
-                    </tr>
-                    {enAtt}
+        let ListView = props.students.map((student) => ( <List student={student} key={student.id}/> ))
+        return (
+            <div className="container">
+                <table className="list">  
+                    <tbody className="list">
+                        <tr>
+                            <th></th>
+                            <th>id</th>
+                            <th>Image</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Spécialité</th>
+                        </tr>
+                        { ListView }
+                    </tbody> 
                 </table>
-    }else {
-        listItems = students.map((student) => <Card student={student}/>)
-    }
+            </div>
+        )
+    } else {
 
-    return (
-        <div className="container">{listItems}</div>
-    )
+        let GridView = props.students.map((student) => ( <Grid student={student} key={student.id}/> ))
+        return (
+            <div className="container">
+                { GridView }
+            </div>
+        )
+    }
 }
 
-export default Cards
+export default Card
