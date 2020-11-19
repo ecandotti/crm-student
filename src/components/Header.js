e9554d2b97cd0dacf27a1cd51670f213f2b6b885
ecandotti <enzo.candotti16@hotmail.com>
2020-11-19 06:31:19 +0100

    Finalisation route

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,37 @@
-//import '../css/ListOfStudents.css'
-import { FiUserPlus } from 'react-icons/fi'
 import { ImList2 } from 'react-icons/im'
 import { BsFillGrid3X3GapFill } from 'react-icons/bs'
-import { AiFillHome } from 'react-icons/ai'
+import { FaUserGraduate, FaUserPlus } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
-
 const Header = (props) => {
-
-    let title = props.title
+    
+    let headerTitle = props.headerTitle
     let nbStudent = props.nbStudent
     let showListGrid = props.showListGrid
-    
+
     return(
         <>
             <div className="row">
 
-                <h3 className="col s12">{title}</h3>
+                <h3 className="col s12">{ headerTitle }</h3>
 
                 <div className="col s6">
-                    Nombre d'étudiant : {nbStudent} trouvés.
+                    Nombre d'étudiant : { nbStudent } trouvés.
                 </div>
                 <div className="col s6 right-align">
                     <Link to="/addstudent">
                         <button className="btn green waves-effect waves-light">
-                            <FiUserPlus />
+                            <FaUserPlus />
                         </button>&#160;
                     </Link>
-                    <Link to="/">
+                    <Link to="/students">
                         <button className="btn blue waves-effect waves-light">
-                            <AiFillHome />
+                            <FaUserGraduate />
                         </button>&#160;
                     </Link>
-                    <button onClick={props.handler_showListGrid} className="btn brown waves-effect waves-light">
+                    <button onClick={ props.handler_showListGrid } className="btn brown waves-effect waves-light">
                         { showListGrid  ? <BsFillGrid3X3GapFill /> : <ImList2 /> }
-                    </button>
+                    </button>&#160;
                 </div>
             </div>
         </>
