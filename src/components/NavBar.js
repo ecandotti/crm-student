4fbdd8a4841b23f349aeddc9275c00bdccb04570
ecandotti <enzo.candotti16@hotmail.com>
2020-11-20 04:43:45 +0100

    Simplification NavBar + info of UserConnexion

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,25 @@
-//import '../css/Menu.css'
 import React from 'react'
 import 'materialize-css'
-import logoSM from  '../img/Logo.svg'
 import UserConnexion from './UserConnexion'
-import imageProfile from '../img/man.svg'
 import { Link } from 'react-router-dom'
 
 
 const NavBar = (props) => {
     
-    let showSubMenu = props.showSubMenu
-    let loginhandler = props.handlerlogin
-    let handlerNav = props.handlerNav
     let user = { 
-        name: "Pierre",
-        lastName: "Leffe",
-        image: { imageProfile }
+        name: "Admin"
     }
 
     return (
         <div className="navbar-fixed">
             <nav>
                 <div className="nav-wrapper blue">
-                    <div className="brand-logo left"> 
-                        <Link to="/students"><img alt="Logo Student Manaager" src={logoSM}/></Link>
+                    <div className="right">
+                        <UserConnexion userInfo={user} history={props.history}/>
                     </div>
-                    <ul className="right hide-on-med-and-down">
-                        <li><UserConnexion showSubMenu={showSubMenu} loginhandler={loginhandler} handlerNav={handlerNav} user={user} /></li>
-                    </ul>
                 </div>
             </nav>
         </div>
-        
     )
 }
 
