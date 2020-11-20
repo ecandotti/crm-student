8bb59f356300aea9c9f0db4f15bbde51a97bc8bf
ecandotti <enzo.candotti16@hotmail.com>
2020-11-20 05:48:37 +0100

    Init isAdmin by props.student

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,9 +6,9 @@ const Grid = (props) => {
 
     const firstName = props.student.firstName
     const lastName = props.student.lastName
-    const speciality = props.student.speciality
-    const id = props.student.id
-    const isAdmin = false
+    const Speciality = props.student.speciality
+    const Id = props.student.id
+    const isAdmin = props.student.isAdmin
 
     const handleSayId = (event) => {
         props.handleStudent(event.target.name)
@@ -25,16 +25,16 @@ const Grid = (props) => {
                         <div className="card-content">
                             <h4 className="header">{ firstName }  { lastName }</h4>
                             <p>
-                                Admin : {isAdmin ? 'OUI' : 'NON'}<br/>
-                                Specialité : { speciality } <br/>
-                                ID : { id }
+                                Admin : { isAdmin ? 'OUI' : 'NON' }<br/>
+                                Specialité : { Speciality } <br/>
+                                ID : { Id }
                             </p>
                         </div>
                         <div className="card-action">
                             <div className="row center-align">
                                 <button className="btn red waves-effect waves-light col s4 offset-s1"><HiTrash /></button> 
                                 {/* <button className="btn disabled col offset-s1 s3"><MdModeEdit /></button> */}
-                                <button onClick={handleSayId} className="btn purple col offset-s1 s4" name={id}><FaAddressCard /></button>
+                                <button onClick={handleSayId} className="btn purple col offset-s1 s4" name={ Id }><FaAddressCard /></button>
                             </div>
                             
                         </div>
--- src/components/List.js
+++ src/components/List.js
@@ -6,8 +6,8 @@ const List = (props) => {
 
     const firstName = props.student.firstName
     const lastName = props.student.lastName
-    const speciality = props.student.speciality
-    const id = props.student.id
+    const Speciality = props.student.speciality
+    const Id = props.student.id
 
     const handleSayId = (event) => {
         props.handleStudent(event.target.name)
@@ -16,15 +16,15 @@ const List = (props) => {
     return(
         <>
             <tr>
-                <td>{ id }</td>
+                <td>{ Id }</td>
                 <td className="center">{ lastName }</td>
                 <td className="center">{ firstName }</td>
-                <td className="center">{ speciality }</td>
+                <td className="center">{ Speciality }</td>
                 <td className="right-align">
                     <div className="row">
                         <button className="btn red waves-effect waves-light col offset-l6 l2 offset-m5 m3"><HiTrash /></button> 
                         {/* <button className="btn disabled col offset-s1 s1"><MdModeEdit /></button>  */}
-                        <button onClick={handleSayId} className="btn purple col l2 offset-l1 m3 offset-m1" name={id}><FaAddressCard /></button>
+                        <button onClick={handleSayId} className="btn purple col l2 offset-l1 m3 offset-m1" name={ Id }><FaAddressCard /></button>
                     </div>
                 </td>
             </tr>
