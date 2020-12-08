40ef4f26985dd07d06032f4dc57798fae0339767
ecandotti <enzo.candotti16@hotmail.com>
2020-12-08 15:08:00 +0100

    Move routes and temporary delete protectedRoutes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import { Login } from './components/Login'
-import { ProtectedRoute } from './protectedRoute'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import 'react-materialize'
-
-function Manage(){
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <ProtectedRoute exact path="/dashboard" component={ App } />
-        <Route path="*" component={() => 'ERREUR 404' } />
-      </Switch>
-    </div>
-  )
-}
+import 'materialize-css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Manage />
-    </BrowserRouter>
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
