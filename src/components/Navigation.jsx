2cdd2b3f34bbe31a18a42b0f2aa61cdbf2ea7263
ecandotti <enzo.candotti16@hotmail.com>
2020-12-10 16:56:46 +0100

    show logout button in header if user is auth

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,34 +1,36 @@
+import React from 'react'
+
 import { FaUserPlus } from 'react-icons/fa'
 import { AiFillHome } from 'react-icons/ai'
 import { NavLink } from 'react-router-dom'
 
 const Navigation = (props) => {
-    
-    let title = 'Student Manager'
-    let nbStudent = props.nbStudent
-
     const ButtonStyle = {
         marginLeft: '5px',
         marginRight: '5px'
     }
+    const HeaderStyle = {
+        marginTop: "50px"
+    }
     return(
         <>
-            <div className="container row">
-
-                <h3 className="col s12">{ title }</h3>
-
-                <div className="col s6">
-                    Nombre d'étudiant : { nbStudent } trouvés.
+            {
+                props.isLoggedIn &&
+                <div className="container row" style={HeaderStyle}>
+                    <div className="col s6">
+                        Nombre d'étudiant : { props.nbStudents } trouvés.
+                    </div>
+                    <div className="col s6 right-align">
+                        <NavLink to="/addstudent" className="btn green waves-effect waves-light" style={ButtonStyle}>
+                            <FaUserPlus />
+                        </NavLink>
+                        <NavLink to="/" className="btn orange waves-effect waves-light" style={ButtonStyle}>
+                            <AiFillHome />
+                        </NavLink>
+                    </div>
                 </div>
-                <div className="col s6 right-align">
-                    <NavLink to="/addstudent" className="btn green waves-effect waves-light" style={ButtonStyle}>
-                        <FaUserPlus />
-                    </NavLink>
-                    <NavLink to="/" className="btn orange waves-effect waves-light" style={ButtonStyle}>
-                        <AiFillHome />
-                    </NavLink>
-                </div>
-            </div>
+            }
+            
         </>
     )
 }
