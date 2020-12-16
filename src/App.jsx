dd7a0bc17c6e214fd5fd6a521c72434c138a67ef
ecandotti <enzo.candotti16@hotmail.com>
2020-12-16 01:15:38 +0100

    delete public page + add DataContext

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { PrivateRoute } from './PrivateRoute'
 
 import { GET_ALL_ID } from './config.json'
 
-import Public from './pages/Public'
 import Login from './pages/Login'
 import AddStudent from './pages/AddStudent'
 import StudentCard from './pages/StudentCard'
@@ -14,17 +13,16 @@ import List from './pages/List'
 
 import Header from './components/Header'
 import Navigation from './components/Navigation'
+import { DataContext } from './context/data'
 
 const App = () => {
 
     const [authTokens, setAuthTokens] = useState('')
-    const [isLoggedIn, setLoggedIn] = useState(false)
     const [allStudents, setAllStudents] = useState([])
 
     const setTokens = (data) => {
         localStorage.setItem("tokens", JSON.stringify(data));
         setAuthTokens(data)
-        setLoggedIn(true)
     }
 
     const updateData = () => {
@@ -42,18 +40,19 @@ const App = () => {
     return (
             <div>
                 <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
-                    <Router>
-                        <Header isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn}/>
-                        <Navigation nbStudents={allStudents.length} isLoggedIn={isLoggedIn}/>
-                        <Switch>
-                            <Route exact path="/public" component={Public} />
-                            <Route exact path="/login" component={() => <Login isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn}/>} />
-                            <PrivateRoute exact path="/" component={() => <List allStudents={allStudents}/> } />
-                            <PrivateRoute exact path="/addstudent" component={AddStudent} />
-                            <PrivateRoute exact path="/student/:id" component={StudentCard} />
-                            <Route path="*" component={() => 'ERREUR 404' } />
-                        </Switch>
-                    </Router>
+                    <DataContext.Provider value={{updateData, allStudents}}>
+                        <Router>
+                            <Header />
+                            <Navigation />
+                            <Switch>
+                                <Route exact path="/login" component={Login} />
+                                <PrivateRoute exact path="/" component={List} />
+                                <PrivateRoute exact path="/addstudent" component={AddStudent} />
+                                <PrivateRoute exact path="/student/:id" component={StudentCard} />
+                                <Route path="*" component={() => 'ERREUR 404' } />
+                            </Switch>
+                        </Router>
+                    </DataContext.Provider>
                 </AuthContext.Provider>
             </div>
             )
