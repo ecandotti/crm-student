85641dd27b697fc1ef13b3813cd694c82a128f23
ecandotti <enzo.candotti16@hotmail.com>
2020-12-16 01:18:56 +0100

    Authentification work with token (use AuthContext)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,45 +1,41 @@
 import React, { useState } from 'react'
-import { Redirect } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
-// import { POST_LOGIN } from '../config.json'
+import { POST_LOGIN } from '../config.json'
 
 import { useAuth } from "../context/auth"
 
-const Login = (props) => {
-    // const [isError, setIsError] = useState(false)
-    const [userName, setUserName] = useState("")
+const Login = () => {
+    const history = useHistory()
+    const [isError, setIsError] = useState(false)
+    const [mail, setMail] = useState("")
     const [password, setPassword] = useState("")
     const { setAuthTokens } = useAuth()
 
     const postLogin = () => {
-        // const val_log = { nom: userName, password: password}
-        // fetch(POST_LOGIN,{
-        //     method: 'POST',
-        //     body: JSON.stringify(val_log),
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }})
-        //     .then(response => response.json())
-        //     .then(req => {
-        //         console.log(req)
-        //         setAuthTokens(req.data)
-        //     })
-        //     .then(setLoggedIn(true))
-        //     .catch(err => setIsError(true))
-        setAuthTokens("oiahfpozihfapoihfpoh")
-        props.setLoggedIn(true)
-      }
-    
-      if (props.isLoggedIn) {
-        return <Redirect to="/" />
+        const loggin = { email: mail, password: password}
+        fetch(POST_LOGIN,{
+            method: 'POST',
+            body: JSON.stringify(loggin),
+            headers: {
+                'Content-Type': 'application/json'
+            }})
+            .then(response => response.json())
+            .then(req => {
+                setAuthTokens(req.token)
+                history.push("/")
+            })
+            .catch(err => setIsError(true))
     }
+
     return(
         <div className="container">
             <div className="nav-wrapper center-align">
                 <h6 className="red-text">Vous devez être authentifié pour accéder à StudentManager</h6>
-                <input type="text" className="center-align" placeholder="username" value={userName} onChange={e => {setUserName(e.target.value) }} />
+                <input type="email" className="center-align" placeholder="username" value={mail} onChange={e => {setMail(e.target.value) }} />
                 <input type="password" className="center-align" placeholder="******" value={password} onChange={e => {setPassword(e.target.value) }} />
-                { /* isError && "The username or password provided were incorrect!" */}
+                { isError && "The username or password provided were incorrect!" }
+                <br/><br/>
                 <button className="btn green" onClick={postLogin}>Se connecter</button>
             </div>
         </div>
